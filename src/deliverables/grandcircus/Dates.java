545d5e8ef7c61d46af0fdfe2a45bcb690825ac1a
Tim Pieniazek <timpieniazek@gmail.com>
2018-03-11 22:20:07 -0400

    Added prompts, scanner input, inital parsing.

--- src/deliverables/grandcircus/Dates.java
+++ src/deliverables/grandcircus/Dates.java
@@ -1,18 +1,32 @@
 package deliverables.grandcircus;
 
+import java.time.LocalDate;
 import java.util.Scanner;
 
 /* Tim Pieniazek - Deliverable 2 */
 
 public class Dates {
-	Scanner scnr = new Scanner(System.in);
-	int firstDate;
-	int secondDate;
-	
-	// TODO: Add prompts and scanner inputs for two dates, using LocalDate
-	
-	// TODO: Store year, month, and day variables for the time between date
-	
-	// TODO: Add output for the variables
-	
+	public static void main(String[] args) {
+		Scanner scnr = new Scanner(System.in);
+		String firstInput;
+		String secondInput;
+		LocalDate firstDate;
+		LocalDate secondDate;
+		
+		System.out.println("Let's calculate the difference in time between two dates!");
+		System.out.println("Please enter the first date:");
+		firstInput = scnr.nextLine();
+		firstDate = LocalDate.parse(firstInput);
+		
+		System.out.println("Please enter the second date:");
+		secondInput = scnr.nextLine();
+		secondDate = LocalDate.parse(secondInput);
+		
+		// TODO: Parse date Strings into LocalDate format
+		
+		// TODO: Store year, month, and day variables for the time between date
+		
+		// TODO: Add output for the variables
+		
+	}
 }
