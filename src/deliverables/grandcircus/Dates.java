c294338325b86b2ee0ea15835cf5c71185d6efd3
Tim Pieniazek <timpieniazek@gmail.com>
2018-03-11 22:01:06 -0400

    Initial commit.

---
+++ src/deliverables/grandcircus/Dates.java
@@ -0,0 +1,18 @@
+package deliverables.grandcircus;
+
+import java.util.Scanner;
+
+/* Tim Pieniazek - Deliverable 2 */
+
+public class Dates {
+	Scanner scnr = new Scanner(System.in);
+	int firstDate;
+	int secondDate;
+	
+	// TODO: Add prompts and scanner inputs for two dates, using LocalDate
+	
+	// TODO: Store year, month, and day variables for the time between date
+	
+	// TODO: Add output for the variables
+	
+}
